e12c0e4eed7f824702d0f9c465426e3dc5283a30
Gabriela <gabsram2975@gmail.com>
2024-02-16 10:35:11 -0600

    03 Adding a simple ListView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,62 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes de flutter'),
+      ),
+      body:  ListView(
+        children: const [Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('****************************************'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60'),
+        Text('KC Campeones del super bowl 58'),
+        Text('KC Campeones del super bowl 59'),
+        Text('KC Campeones del super bowl 60')]
+      ,)
+    );
   }
 }
