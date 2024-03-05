fa45d234c0ba773439c47eb6958f27a3483acd86
Gabriela <gabsram2975@gmail.com>
2024-03-05 12:01:56 -0600

    14 Images

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/theme/app_theme.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class ImagesScreen extends StatefulWidget {
   const ImagesScreen({super.key});
@@ -14,11 +16,62 @@ class _ImagesScreenState extends State<ImagesScreen> {
       appBar: AppBar(
         title: const Text('Imagenes'),
       ),
-      body: const Column(
+      body: ListView(
         children: [
-          Image(image: AssetImage('assets/img/logoInt.png'),)
+          cardImage1(),
+          image2(),
         ],
       )
+    );
+  }
+  Card cardImage1(){
+    return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+          ),
+          margin: const EdgeInsets.all(15),
+          elevation: 15,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Column(children: [
+          const SizedBox(
+            height: 300.0,
+            width: 300.0,
+            child:  Image(
+              image: AssetImage('assets/img/logoInt.png'),
+              ),
+          ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Logo de WaterCheck',
+                style: AppTheme.lightTheme.textTheme.headlineSmall,
+              ),
+            )
+        ],)
+        ) ,
+    );
+  }
+  SizedBox image2(){
+    return SizedBox(
+      height: 200.0,
+      width: 200.0,
+      child:  Image.network('https://juegoyaprendo.guanajuato.gob.mx/imagenes/cuida/CUIDADO%20DEL%20AGUA/Enmascarar%20grupo%2032.png'),
+    );
+  }
+  Stack imageFade(){
+    return Stack(
+      children: <Widget>[
+        const Center(
+        child: CircularProgressIndicator(),
+        ),
+        Center(
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage, 
+            image: 'https://juegoyaprendo.guanajuato.gob.mx/imagenes/cuida/CUIDADO%20DEL%20AGUA/Enmascarar%20grupo%2032.png')
+
+        )
+      ],
     );
   }
 }
