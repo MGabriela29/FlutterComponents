5b9064f1e3e371566e7d76a7627e5ba48fdb1a08
Gabriela <gabsram2975@gmail.com>
2024-03-08 09:13:15 -0600

    17 Fuctional ButtonNavigationBar

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/screens/home_screen.dart';
+import 'package:practica3/screens/images_screen.dart';
+import 'package:practica3/screens/infinite_scroll_screen.dart';
+import 'package:practica3/screens/notifications_screen.dart';
+
 import 'package:practica3/theme/app_theme.dart';
 
 class InputsScreen extends StatefulWidget {
@@ -15,6 +20,34 @@ class _InputsScreenState extends State<InputsScreen> {
   bool checkboxValue1 = false;
   bool checkboxValue2 = false;
   bool checkboxValue3 = false;
+  int indexNavigation = 0 ;
+
+  openScreen(int index, BuildContext context){//variavle buildcontext puede que no la pida pero es mejor enviarla
+  MaterialPageRoute ruta = MaterialPageRoute( // variable para la ruta
+    builder: (context) => const HomeScreen());
+  switch(index){
+    case 0: 
+      ruta = MaterialPageRoute( // variable para la ruta
+      builder: (context) => const HomeScreen());
+      break;
+    case 1: 
+      ruta = MaterialPageRoute( // variable para la ruta
+      builder: (context) => const InfiniteScrollScreen());  
+      break;
+    case 2:
+      ruta = MaterialPageRoute( // variable para la ruta
+      builder: (context) => const NotificationsScreen());
+      break;
+    case 3: 
+      ruta = MaterialPageRoute( // variable para la ruta
+      builder: (context) => const ImagesScreen());    
+      break;
+  }
+  setState(() {
+    indexNavigation = index; 
+    Navigator.push(context, ruta);
+  });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,14 +82,26 @@ class _InputsScreenState extends State<InputsScreen> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: AppTheme.primaryColor,
+        currentIndex: indexNavigation,
+          unselectedItemColor: Colors.amber,
+          selectedItemColor: Colors.blue,
+          onTap: (index)=> openScreen(index, context),//para ver en que ventana estoy // openScreen metodo es para las rutas
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Inicio', ),
+            label: "Home", ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.add_comment),
-              label: 'Comentarios')
+              label: 'Comentarios'),
+               BottomNavigationBarItem(
+               icon: Icon(Icons.exit_to_app_outlined),
+               label: "notificaciones",
+              ),
+              BottomNavigationBarItem(
+               icon: Icon(Icons.exit_to_app_outlined),
+               label: "images",
+              ),
         ]
          ),
     );
